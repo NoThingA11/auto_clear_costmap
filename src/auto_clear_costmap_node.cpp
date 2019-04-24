4bb23e9009986e536c81208ec4a3f9c2f26125f9
NoThingA11 <44485035+NoThingA11@users.noreply.github.com>
2019-04-24 11:49:01 +0700

    Add files via upload

---
+++ src/auto_clear_costmap_node.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include <std_srvs/Empty.h>
+
+int main(int argc, char **argv){
+	ROS_INFO("Test Clear Costmap");
+	ros::init(argc, argv, "clear_costmap_service");
+	ros::NodeHandle nh;
+	ros::service::waitForService("/move_base/clear_costmaps");
+	ros::ServiceClient clearClient = nh.serviceClient<std_srvs::Empty>("/move_base/clear_costmaps");
+    std_srvs::Empty srv;
+	ros::Time begin = ros::Time::now();
+  
+	//ros::Rate loop_rate(1);
+
+	while(ros::ok()){
+		clearClient.call(srv);
+		ROS_INFO("Clear Costmap");
+		ros::Duration(3).sleep();
+	}
+	
+}
